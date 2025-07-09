61bff2aad9e0765b37691215feb0ed73f466acc9
Mike Valeriano <mkvlrn@proton.me>
2025-07-09 15:18:09 -0300

    feat: add delete-customer usecase

---
+++ internal/domain/src/usecases/delete-customer.test.ts
@@ -0,0 +1,64 @@
+import { afterEach, assert, beforeEach, describe, it, vi } from "vitest";
+import { validCustomer } from "#/__fixtures__/customer.fixtures.ts";
+import { mockCustomerRepository } from "#/__fixtures__/repositories.fixtures.ts";
+import { DeleteCustomerUseCase } from "#/usecases/delete-customer.ts";
+import { AppError } from "#/utils/app-error.ts";
+import { Result } from "#/utils/result.ts";
+
+let usecase: DeleteCustomerUseCase;
+
+beforeEach(() => {
+  usecase = new DeleteCustomerUseCase(mockCustomerRepository);
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+it("deletes customer", async () => {
+  vi.spyOn(mockCustomerRepository, "findById").mockResolvedValue(Result.ok(validCustomer));
+  vi.spyOn(mockCustomerRepository, "delete").mockResolvedValue(Result.ok(undefined));
+
+  const result = await usecase.execute(validCustomer.id);
+
+  assert.isUndefined(result.error);
+  assert.isUndefined(result.value);
+});
+
+it("returns error if customer is not registered", async () => {
+  vi.spyOn(mockCustomerRepository, "findById").mockResolvedValue(Result.ok(undefined));
+
+  const result = await usecase.execute(validCustomer.id);
+
+  assert.isDefined(result.error);
+  assert.instanceOf(result.error, AppError);
+  assert.strictEqual(result.error.name, "INEXISTENT");
+  assert.strictEqual(result.error.message, `customer with id '${validCustomer.id}' not registered`);
+});
+
+describe("returns error if repository throws", () => {
+  it("when checking if customer exists", async () => {
+    const repoError = new AppError("REPOSITORY_ERROR", "database exploded");
+    vi.spyOn(mockCustomerRepository, "findById").mockResolvedValue(Result.error(repoError));
+
+    const result = await usecase.execute(validCustomer.id);
+
+    assert.isDefined(result.error);
+    assert.instanceOf(result.error, AppError);
+    assert.strictEqual(result.error.name, "REPOSITORY_ERROR");
+    assert.strictEqual(result.error.message, "database exploded");
+  });
+
+  it("when deleting customer", async () => {
+    vi.spyOn(mockCustomerRepository, "findById").mockResolvedValue(Result.ok(validCustomer));
+    const repoError = new AppError("REPOSITORY_ERROR", "database exploded");
+    vi.spyOn(mockCustomerRepository, "delete").mockResolvedValue(Result.error(repoError));
+
+    const result = await usecase.execute(validCustomer.id);
+
+    assert.isDefined(result.error);
+    assert.instanceOf(result.error, AppError);
+    assert.strictEqual(result.error.name, "REPOSITORY_ERROR");
+    assert.strictEqual(result.error.message, "database exploded");
+  });
+});
---
+++ internal/domain/src/usecases/delete-customer.ts
@@ -0,0 +1,37 @@
+import type { CustomerRepository } from "#/repositories/customer.repository.ts";
+import { AppError } from "#/utils/app-error.ts";
+import { Result } from "#/utils/result.ts";
+
+export class DeleteCustomerUseCase {
+  private readonly repository: CustomerRepository;
+
+  constructor(repo: CustomerRepository) {
+    this.repository = repo;
+  }
+
+  async execute(id: string): Promise<Result<undefined>> {
+    const exists = await this.checkIfExists(id);
+    if (exists.error) {
+      return Result.error(exists.error);
+    }
+
+    const result = await this.repository.delete(id);
+    if (result.error) {
+      return Result.error(new AppError("REPOSITORY_ERROR", result.error.message));
+    }
+
+    return Result.ok(undefined);
+  }
+
+  private async checkIfExists(id: string): Promise<Result<undefined>> {
+    const exists = await this.repository.findById(id);
+    if (exists.error) {
+      return Result.error(new AppError("REPOSITORY_ERROR", exists.error.message));
+    }
+    if (!exists.value) {
+      return Result.error(new AppError("INEXISTENT", `customer with id '${id}' not registered`));
+    }
+
+    return Result.ok(undefined);
+  }
+}
