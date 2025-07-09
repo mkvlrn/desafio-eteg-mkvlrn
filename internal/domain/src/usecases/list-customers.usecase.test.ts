15b6c992beea91e048aa27cfee5e911ae473a710
Mike Valeriano <mkvlrn@proton.me>
2025-07-09 14:57:00 -0300

    feat: add list-customers usecase

---
+++ internal/domain/src/usecases/list-customers.usecase.test.ts
@@ -0,0 +1,37 @@
+import { afterEach, assert, beforeEach, it, vi } from "vitest";
+import { validCustomer } from "#/__fixtures__/customer.fixtures.ts";
+import { mockCustomerRepository } from "#/__fixtures__/repositories.fixtures.ts";
+import { ListCustomersUseCase } from "#/usecases/list-customers.usecase.ts";
+import { AppError } from "#/utils/app-error.ts";
+import { Result } from "#/utils/result.ts";
+
+let usecase: ListCustomersUseCase;
+
+beforeEach(() => {
+  usecase = new ListCustomersUseCase(mockCustomerRepository);
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+it("returns customers", async () => {
+  vi.spyOn(mockCustomerRepository, "findAll").mockResolvedValue(Result.ok([validCustomer]));
+
+  const result = await usecase.execute();
+
+  assert.isUndefined(result.error);
+  assert.deepStrictEqual(result.value, [validCustomer]);
+});
+
+it("returns error if repository throws", async () => {
+  const repoError = new AppError("REPOSITORY_ERROR", "database exploded");
+  vi.spyOn(mockCustomerRepository, "findAll").mockResolvedValue(Result.error(repoError));
+
+  const result = await usecase.execute();
+
+  assert.isDefined(result.error);
+  assert.instanceOf(result.error, AppError);
+  assert.strictEqual(result.error.name, "REPOSITORY_ERROR");
+  assert.strictEqual(result.error.message, "database exploded");
+});
---
+++ internal/domain/src/usecases/list-customers.usecase.ts
@@ -0,0 +1,21 @@
+import type { CustomerDto } from "#/models/customer.ts";
+import type { CustomerRepository } from "#/repositories/customer.repository.ts";
+import { Result } from "#/utils/result.ts";
+
+export class ListCustomersUseCase {
+  private readonly repository: CustomerRepository;
+
+  constructor(repo: CustomerRepository) {
+    this.repository = repo;
+  }
+
+  async execute(): Promise<Result<CustomerDto[]>> {
+    const result = await this.repository.findAll();
+
+    if (result.error) {
+      return Result.error(result.error);
+    }
+
+    return Result.ok(result.value);
+  }
+}
