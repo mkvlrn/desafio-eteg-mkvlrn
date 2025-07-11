e54b6509772ba38aeef58fff1c598173c52e6a7d
Mike Valeriano <mkvlrn@proton.me>
2025-07-11 01:21:28 -0300

    feat: adjust admin table colors

--- apps/frontend/src/pages/admin/table.tsx
+++ apps/frontend/src/pages/admin/table.tsx
@@ -19,8 +19,8 @@ export function Table({ title, data }: TableProps) {
     <>
       <h1 className="mt-2 text-center text-2xl">{title}</h1>
       <div className="container mb-6 max-h-64 w-full overflow-auto">
-        <table className="w-full truncate whitespace-nowrap text-left text-gray-500 text-sm rtl:text-right dark:text-gray-400">
-          <thead className="bg-gray-50 text-gray-700 text-xs uppercase dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full truncate whitespace-nowrap text-left text-c3 text-sm">
+          <thead className="bg-gray-50 text-gray-700 text-xs uppercase dark:bg-c3 dark:text-c1">
             <tr>
               {headers.slice(1).map((header) => (
                 <th className="px-6 py-3" key={header} scope="col">
@@ -32,7 +32,7 @@ export function Table({ title, data }: TableProps) {
           <tbody>
             {rows.map((row) => (
               <tr
-                className="border-gray-200 border-b bg-white dark:border-gray-700 dark:bg-gray-800"
+                className="border-gray-200 border-b bg-white dark:border-gray-700 dark:bg-c1"
                 key={JSON.stringify(row)}
               >
                 {row.slice(1).map((col) => (
